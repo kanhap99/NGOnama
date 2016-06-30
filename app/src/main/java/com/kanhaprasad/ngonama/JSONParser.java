b9e8da1d6b155402652a538b24af44d7c943c70c
Kanha Prasad <41562@jisedu.or.id>
2016-06-30 15:09:12 +0530

    back button functionality working

---
+++ app/src/main/java/com/kanhaprasad/ngonama/JSONParser.java
@@ -0,0 +1,8 @@
+package com.kanhaprasad.ngonama;
+
+/**
+ * Created by 41562 on 6/21/16.
+ */
+public class JSONParser {
+
+}
--- app/src/main/java/com/kanhaprasad/ngonama/MainActivity.java
+++ app/src/main/java/com/kanhaprasad/ngonama/MainActivity.java
@@ -1,16 +1,16 @@
 package com.kanhaprasad.ngonama;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.graphics.Color;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
@@ -18,26 +18,16 @@ import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
 
 public class MainActivity extends AppCompatActivity {
-    TextView about, events,search,blog;
+    //declarations
+    private WebView about,events,blog, search;
+    private WebView webViews[] = new WebView[4];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        about = (TextView) findViewById(R.id.about_us);
-        events = (TextView) findViewById(R.id.events);
-        search = (TextView) findViewById(R.id.search);
-        blog = (TextView) findViewById(R.id.blog);
-
-        about.setText(Html.fromHtml("<p> NGONAMA is a revolutionary initiative of Digital Empowerment Foundation. As part of its commitment to sustainable efforts of social and digital upliftment activities, Digital Empowerment Foundation aims to provide a platform to participate and share the knowledge, skills, experiences and good governance across the civil society sector.</p>" +
-                "<p>\n" +
-                "The platform is designed to bring together NGOs, volunteers, Individuals and government and corporate foundations at the same platform to engage with each other. NGONAMA is a platform for NGOs to connect, discover, engage, learn and share through communicating with other NGOs across boundaries through openness.</p>" +
-                "The central idea of NGONAMA is to build an interactive platform for NGOs to collaborate amongst themselves through personal outreach.\n" +
-                "\n" +
-                "<p>The online portal will provides a channel to people who are interested in contributing in social activities through their participation and expertise.  NGONAMA provides NGOs with a platform to interact with a global community and participate in various decision-making and programs leading to a be a better world, a world of economic and social justice.</p>"));
-
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
         AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
@@ -53,13 +43,13 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation.addItem(item4);
 
 // Set background color
-        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#C3C6C7"));
 
 // Disable the translation inside the CoordinatorLayout
         bottomNavigation.setBehaviorTranslationEnabled(false);
 
 // Change colors
-        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
+        bottomNavigation.setAccentColor(Color.parseColor("#97D734"));
         bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
 
 // Force to tint the drawable (useful for font with icon for example)
@@ -75,13 +65,40 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigation.setCurrentItem(0);
 
 // Customize notification (title, background, typeface)
-        bottomNavigation.setNotificationBackgroundColor(Color.parseColor("#F63D2B"));
+        bottomNavigation.setNotificationBackgroundColor(Color.parseColor("#C3C6C7"));
 
 // Add or remove notification for each item
         bottomNavigation.setNotification("4", 1);
         bottomNavigation.setNotification("", 1);
+//
 
-// Set listener
+        //final WebView webview = new WebView(this);
+        //configuring the views
+
+        about = (WebView) findViewById(R.id.about_us);
+        events = (WebView) findViewById(R.id.events);
+        search = (WebView) findViewById(R.id.search);
+        blog = (WebView) findViewById(R.id.blog);
+
+        webViews[0] = about;
+        webViews[1] = events;
+        webViews[2] = search;
+        webViews[3] = blog;
+
+        for(WebView webview: webViews) {
+            configWebView(webview);
+        }
+        /*String aboutContent = "<html><body><p> NGONAMA is a revolutionary initiative of Digital Empowerment Foundation. As part of its commitment to sustainable efforts of social and digital upliftment activities, Digital Empowerment Foundation aims to provide a platform to participate and share the knowledge, skills, experiences and good governance across the civil society sector.</p>"+
+                "The platform is designed to bring together NGOs, volunteers, Individuals and government and corporate foundations at the same platform to engage with each other. NGONAMA is a platform for NGOs to connect, discover, engage, learn and share through communicating with other NGOs across boundaries through openness.</p>"+
+                "The central idea of NGONAMA is to build an interactive platform for NGOs to collaborate amongst themselves through personal outreach."+
+                "<p>The online portal will provides a channel to people who are interested in contributing in social activities through their participation and expertise.  NGONAMA provides NGOs with a platform to interact with a global community and participate in various decision-making and programs leading to a be a better world, a world of economic and social justice.</p></body></html>";
+
+        about.loadData(aboutContent, "text/html",null);*/
+        about.loadUrl("http://ngonama.org/about-us");
+        events.loadUrl("http://ngonama.org/activity");
+        search.loadUrl("http://ngonama.org/search-ngo");
+        blog.loadUrl("http://ngonama.org/blog");
+        // Set listener
         bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
             public boolean onTabSelected(int position, boolean wasSelected) {
@@ -89,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
+       // webview.loadUrl("http://ngonama.org/search-ngo");
     }
 
     @Override
@@ -113,17 +130,45 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (events.isFocused() && events.canGoBack())
+            events.goBack();
+        else if (blog.isFocused() && blog.canGoBack())
+            blog.goBack();
+        else if (search.isFocused() && search.canGoBack())
+            search.goBack();
+        else
+            super.onBackPressed();
+    }
     private void toggle(int position){
-        TextView views[] = {about, events,search,blog};
+        WebView views[] = {about, events, search, blog};
         int count = 0;
-        for(TextView view: views) {
+        for(WebView view: views) {
             if(position == count)
                 view.setVisibility(View.VISIBLE);
             else
                 view.setVisibility(View.INVISIBLE);
             count++;
         }
+    }
+    private void configWebView(final WebView webview) {
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                webview.loadUrl("javascript:  (function (){ " +
+                        "document.getElementsByTagName('header')[0].style.display=\"none\";" +
+                        "document.getElementById('header-title').style.display=\"none\";"+
+                        "document.getElementById('header-very-top').style.display=\"none\";"+
+                        "document.getElementById('sidebar').style.display=\"none\";"+
+                        "document.getElementsByClassName('footer-bottom')[0].style.display=\"none\";"+
+                        "document.getElementById('whats-new-form').style.display=\"none\";"+
+                        "}) () ");
 
-
+            }
+        });
     }
 }
